9af6ebd725c9375f66c2207f740147e8b9b381f2
hanif365 <m.a.hanifkhan64@gmail.com>
2024-01-30 20:34:17 +0600

    Deploy application in vercel and use CICD.

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <h1>Hello Developers, Now we are learning cicd and docker..</h1>
+      <h1>
+        Hello Developers, Now we are learning cicd and docker. Now we checking
+      </h1>
     </main>
   );
 }
