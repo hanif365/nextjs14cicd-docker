2c2bd9eafd3e73a2b1eb9ac33c894ed37308ab6a
hanif365 <m.a.hanifkhan64@gmail.com>
2024-01-30 20:48:04 +0600

    Use github workflow and cicd 2.

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
     <main>
       <h1>
         Hello Developers, Now we are learning cicd and docker. Now we checking
-        it.
+        it. It's really awesome.
       </h1>
     </main>
   );
